462988a573292aeb57f540705d4917e1a7a5319c
Joren Van Onder <joren.vanonder@gmail.com>
2019-04-14 19:23:41 -0700

    Add style

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-(async() => {
+{
     const submitButton = document.querySelector("#submit");
     const input = document.querySelector("#input");
 
     console.log(submitButton);
 
-    submitButton.addEventListener("click", event => {
+    submitButton.addEventListener("click", async event => {
         event.preventDefault();
-        fetch("http://localhost:8080", {
+        let response = await fetch("http://localhost:8080", {
             method: "POST",
             body: JSON.stringify({ link: input.value })
-        }).then(response => {
-            response.json().then(json => console.log(json));
         });
+
+        let json = await response.json();
+        console.log(json);
     });
-})();
+}
