c3e7f6d79a4f02c8cc4a39d46205835c7957cf4a
Joren Van Onder <joren.vanonder@gmail.com>
2019-04-14 20:56:44 -0700

    Display result

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 {
     const submitButton = document.querySelector("#submit");
-    const input = document.querySelector("#input");
-
-    console.log(submitButton);
+    const input = document.querySelector("#link");
+    const result = document.querySelector("#result");
+    const shortened = document.querySelector("#shortened-link");
 
     submitButton.addEventListener("click", async event => {
         event.preventDefault();
-        let response = await fetch("http://localhost:8080", {
+
+        submitButton.disabled = true;
+        result.style.visibility = "hidden";
+        result.style.webkitAnimation = "";
+
+        const response = await fetch("http://localhost:8080", {
             method: "POST",
             body: JSON.stringify({ link: input.value })
         });
 
-        let json = await response.json();
-        console.log(json);
+        const json = await response.json();
+
+        // correctly handle current URL ending with / and link starting with /
+        const shortenedURL =
+            window.location.href.replace(/\/$/, "") +
+            "/" +
+            json.link.replace(/^\//, "");
+        shortened.innerHTML = shortenedURL;
+
+        result.style.webkitAnimation = "none";
+        setTimeout(function() {
+            result.style.webkitAnimation = "";
+            result.style.visibility = "visible";
+            submitButton.disabled = false;
+        }, 5);
     });
 }
