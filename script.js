6bb4ea680af6f62f9d93ad54302b1e058ed55edd
Joren Van Onder <joren.vanonder@gmail.com>
2019-04-16 22:16:41 -0700

    Show "Copied!" toast

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
     const input = document.querySelector("#link");
     const result = document.querySelector("#result");
     const shortened = document.querySelector("#shortened-link");
+    const toast = document.querySelector("#toast");
 
     submitButton.addEventListener("click", async event => {
         event.preventDefault();
@@ -35,7 +36,13 @@
     });
 
     const clipboard = new ClipboardJS("#copy");
-    clipboard.on('success', (event) => {
+    clipboard.on("success", event => {
         event.clearSelection();
+        toast.style.webkitAnimation = "none";
+        setTimeout(function() {
+            toast.style.webkitAnimation = "";
+            toast.style.visibility = "visible";
+            submitButton.disabled = false;
+        }, 20);
     });
 }
